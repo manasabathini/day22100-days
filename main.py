0474b7b5e7e3b81603c9fd0d15c4650f2da27ef9
manasabathini <33703157+manasabathini@users.noreply.github.com>
2023-06-21 20:52:51 +0000

    Added code

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import random
 
-for i in range(10):
-  myNumber = random.randint(1, 100)
-  print(myNumber)
+print("Totally Random One-Million-to-One")
+
+attempt = 1
+randomNumber = random.randint(1, 100000)
+print(randomNumber)
+while True:
+  guessNumber = int(input("pick a number between 1 and 100000: "))
+
+  if guessNumber < randomNumber:
+    print("too low")
+    attempt += 1
+  elif guessNumber > randomNumber:
+    print("too high")
+    attempt += 1
+    continue 
+  elif guessNumber == randomNumber:
+    print("Awesome. Correct number! 🥳")
+    break
+    exit()
+  else:
+    print("number is not in range")
+print("it took you", attempt, "attempt(s) to get the correct answer.")
